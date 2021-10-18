2de9d9aa1b0150c2183aef79cd00c609a9168118
Veysel Karaca <57874373+veyselkaraca@users.noreply.github.com>
2021-10-19 00:02:05 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,25 @@
+console.log("Hello World.");
+
+
+let dolarBugun=9.30
+let dolarDun=9.20
+console.log(dolarDun)
+const euroDun=11.2
+//euroDun=11
+
+//array
+//camelCase
+//PascalCase
+let konutkredileri=["Konut kredisi","Emlak Konut kredisi","Kamu Konut kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutkredileri.length; i++) {
+    console.log("<li>"+konutkredileri[i]+"<li>")
+}
+console.log("<ul>")
+{/* <ul>
+    <li>Konut kredisi</li>
+    <li>Emlak Konut kredisi</li>
+    <li>Kamu Konut kredisi</li>
+</ul> */}
+console.log(konutkredileri)
